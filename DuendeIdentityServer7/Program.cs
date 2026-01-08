f5104b748d51775864362856742026ed03189a97
Tore Nestenius <tore@tn-data.se>
2026-01-08 15:14:30 +0100

    Add Duende IdentityServer 7 sample code

---
+++ DuendeIdentityServer7/Program.cs
@@ -0,0 +1,74 @@
+using IdentityServerHost;
+using IdentityServerInMem;
+using Serilog;
+using Serilog.Events;
+using System.Globalization;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .Enrich.FromLogContext()
+    .WriteTo.Console(formatProvider: CultureInfo.InvariantCulture)
+    .CreateBootstrapLogger();
+
+try
+{
+    Log.Information("Starting host...");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddRazorPages();
+
+    builder.Services.AddSerilog((services, lc) =>
+    {
+        lc.MinimumLevel.Information()
+          .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+          .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+          .MinimumLevel.Override("System", LogEventLevel.Warning)
+          .MinimumLevel.Override("Duende", LogEventLevel.Debug)
+          .ReadFrom.Services(services)
+          .Enrich.FromLogContext()
+          .WriteTo.Console();
+    });
+
+    builder.Services.AddAuthentication();
+    builder.Services.AddAuthorization();
+
+    var isBuilder = builder.Services.AddIdentityServer(options =>
+    {
+        options.Events.RaiseErrorEvents = true;
+        options.Events.RaiseInformationEvents = true;
+        options.Events.RaiseFailureEvents = true;
+        options.Events.RaiseSuccessEvents = true;
+    }).AddTestUsers(TestUsers.Users);
+
+    // in-memory, code config
+    isBuilder.AddInMemoryIdentityResources(Config.IdentityResources);
+    isBuilder.AddInMemoryApiScopes(Config.ApiScopes);
+    isBuilder.AddInMemoryClients(Config.Clients);
+
+
+    var app = builder.Build();
+
+    app.UseStaticFiles();
+    app.UseSerilogRequestLogging();
+
+    app.UseRouting();
+
+    app.UseIdentityServer();
+
+    app.UseAuthorization();
+
+    app.MapRazorPages()
+        .RequireAuthorization();
+
+    app.Run();
+}
+catch (Exception ex) when (ex is not HostAbortedException)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
---
+++ DuendeIdentityServer7/README.md
@@ -0,0 +1,5 @@
+## Setting Up Duende IdentityServer 7 From Scratch
+This folder contains the demo code for the blog post about setting up Duende IdentityServer 7.4.4 from scratch.
+
+See my blog posts for more details at https://nestenius.se and for training workshops visit https://tn-data.se/courses/
+
